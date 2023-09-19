60fd51e032279347ac5b3656fa27989baad28fce
lineekz <119862822+lineekz@users.noreply.github.com>
2023-09-19 23:47:56 +0300

    Add files via upload

--- project/main.py
+++ project/main.py
@@ -16,29 +16,31 @@ def read_json():
         dt = json.load(f)
         birg = dt["birg"]
         val = dt["val"]
-        return birg, val
+        time = dt["time"]
+        return birg, val, time
 
-def write_json(birg: str, val: str):
-    with open('dt.json', 'w') as fuck:
-        json.dump({"birg": birg, "val": val}, fuck)
+def write_json(birg: str, val: str, time: str):
+    with open('dt.json', 'w') as d:
+        json.dump({"birg": birg, "val": val, "time": time}, d)
 
-birg, val = read_json()
+birg, val, time = read_json()
 
 with st.sidebar:
+    st.subheader("Select cryptocurrency")
     c1, c2, c3 = st.columns(3)
     with c1:
         img = Image.open("project/bitcoin.png")
         st.image(img)
         click_BTC = st.button("BTC/USD")
         if click_BTC:
-            write_json(birg, "BTC")
+            write_json(birg, "BTC", time)
             st.experimental_rerun()
-        st.divider()
+
         img = Image.open("project/coin.png")
         st.image(img)
         click_SOL = st.button("SOL/USD")
         if click_SOL:
-            write_json(birg, "SOL")
+            write_json(birg, "SOL", time)
             st.experimental_rerun()
 
     with c2:
@@ -46,15 +48,14 @@ with st.sidebar:
         st.image(img)
         click_ETH = st.button("ETH/USD")
         if click_ETH:
-            write_json(birg, "ETH")
+            write_json(birg, "ETH", time)
             st.experimental_rerun()
-        st.divider()
 
         img = Image.open("project/bnb.png")
         st.image(img)
         click_BNB = st.button("BNB/USD")
         if click_BNB:
-            write_json(birg, "BNB")
+            write_json(birg, "BNB", time)
             st.experimental_rerun()
 
     with c3:
@@ -62,31 +63,49 @@ with st.sidebar:
         st.image(img)
         click_LTC = st.button("LTC/USD")
         if click_LTC:
-            write_json(birg, "LTC")
+            write_json(birg, "LTC", time)
             st.experimental_rerun()
-        st.divider()
 
         img = Image.open("project/currency.png")
         st.image(img)
         click_XRP = st.button("ADA/USD")
         if click_XRP:
-            write_json(birg, "ADA")
+            write_json(birg, "ADA", time)
             st.experimental_rerun()
 
-with col2:
-    img = Image.open("project/binance.png")
-    st.image(img)
-    click = st.button("Binance")
-    if click:
-        write_json("Binance", val)
-        st.experimental_rerun()
+    st.divider()
+    st.subheader("Select a time interval")
+    but1, but2, but3 = st.columns(3)
+    with but1:
+        bro1 = st.button("Minute", key="345jkh6")
+        if bro1:
+            write_json(birg, val, "minute")
+            st.experimental_rerun()
+    with but2:
+        bro2 = st.button("Hour", key="l3452ad")
+        if bro2:
+            write_json(birg, val, "hour")
+            st.experimental_rerun()
+    with but3:
+        bro3 = st.button("Day", key="sd211s")
+        if bro3:
+            write_json(birg, val, "day")
+            st.experimental_rerun()
+
+    with col2:
+        img = Image.open("project/binance.png")
+        st.image(img)
+        click = st.button("Binance")
+        if click:
+            write_json("Binance", val, time)
+            st.experimental_rerun()
 
 with col4:
     img2 = Image.open("project/bybit.png")
     st.image(img2)
     click2 = st.button("Bybit")
     if click2:
-        write_json("bybit", val)
+        write_json("bybit", val, time)
         st.experimental_rerun()
 
 with col3:
@@ -94,7 +113,7 @@ with col3:
     st.image(img1)
     click1 = st.button("Kucoin")
     if click1:
-        write_json("Kucoin", val)
+        write_json("Kucoin", val, time)
         st.experimental_rerun()
 
 with col1:
@@ -114,8 +133,7 @@ async def cryptocompare():
             "action": "SubAdd",
             "subs": [f"2~{birg}~{val}~USDT"],
         }))
-        print(val)
-        print(birg)
+
         with st.empty():
             while True:
                 try:
@@ -130,7 +148,7 @@ async def cryptocompare():
                         if data["TYPE"] == "2" and 'PRICE' in data:
                             st.metric("Current Price", data["PRICE"])
                     r = requests.get(
-                        f'https://min-api.cryptocompare.com/data/v2/histominute?fsym={val}&tsym=USDT&e={birg}&limit=1&api_key={ak1}')
+                        f'https://min-api.cryptocompare.com/data/v2/histo{time}?fsym={val}&tsym=USDT&e={birg}&limit=1&api_key={ak1}')
                     rd = json.loads(r.text)
                     with c2:
                         co1, co2 = st.columns(2)
